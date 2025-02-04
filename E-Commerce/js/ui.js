54862b4c65ddc16be16819b6f50d279ab435375f
Asemanijoshua <joshuaasemani27@gmail.com>
2025-02-04 22:10:03 +0000

    Finished with the design and left with the functionalities

---
+++ E-Commerce/js/ui.js
@@ -0,0 +1,9 @@
+
+const container = document.querySelector('.details-group-example');
+
+// Close all other details when one is shown
+container.addEventListener('sl-show', event => {
+  if (event.target.localName === 'sl-details') {
+    [...container.querySelectorAll('sl-details')].map(details => (details.open = event.target === details));
+  }
+});
